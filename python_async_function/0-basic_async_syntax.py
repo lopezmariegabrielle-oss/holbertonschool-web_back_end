af8e656e9fefb9e26d96a78d1ecbb8e2533fa71b
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-16 10:28:47 +0200

    Add task

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Ce module fournit une coroutine asynchrone simple pour expérimenter
+les bases du fonctionnement de asyncio et des délais aléatoires.
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Attend un délai aléatoire entre 0 et max_delay secondes,
+    puis retourne ce délai.
+    """
+    random.uniform(0, max_delay)
+    delai = random.uniform(0, max_delay)
+    await asyncio.sleep(delai)
+    return delai
