cc5d7d82275906d1c17b0835d37d2ca47713bd3f
Marie Lopez <lopezmariegabrielle@gmail.com>
2026-06-18 14:54:09 +0200

    Add task

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -14,12 +14,12 @@ async def measure_runtime() -> float:
     Measures the total execution time of running async_comprehension
     four times concurrently using asyncio.gather, then returns the duration.
     """
-    start_time = time.perf_counter()
+    start_time = time.time()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    end_time = time.perf_counter()
+    end_time = time.time()
     return end_time - start_time
